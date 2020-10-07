c815a6129a6bbe58098359ef507142c422d5054b
Oluwafunmito Blessed <contact@funmitoblessed.com>
2020-10-07 12:39:08 +0100

    Add Product Description Component

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product-page',
+  selector: 'app-product-page', 
   templateUrl: './product-page.component.html',
   styleUrls: ['./product-page.component.css']
 })
